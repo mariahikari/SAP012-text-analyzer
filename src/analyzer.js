3a7a8bb219d52863e9283213eb98ef0f5f5850c2
Maria Hikari <mariahikariyps@gmail.com>
2024-01-21 20:54:26 -0300

    commit

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,22 +1,64 @@
-const analyzer = {  
+
+
+const analyzer = {
   getWordCount: (text) => {
     //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
+    const qntpalavras = text.trim().split(' '); 
+    if (text === ""){
+      return 0
+    }
+    return qntpalavras.length;
+
   },
+
   getCharacterCount: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
+    return text.length;
   },
   getCharacterCountExcludingSpaces: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
+    const excluindoespaco = text.replace(/[\s.:,;!]/g, '');
+    return excluindoespaco.length;
+
   },
-  getAverageWordLength: (text) => {    
+  getAverageWordLength: (text) => {
     //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
+    const qntpalavras = text.split(' ');
+    let sum = 0;
+    for (let i = 0; i < qntpalavras.length; i++) {
+      sum += qntpalavras[i].length;
+    }
+
+    const comprimentomedio = (sum / qntpalavras.length).toFixed(2);
+    return Number(comprimentomedio);
+
   },
   getNumberCount: (text) => {
     //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
+    const qntpalavras = text.replace(/.$/, ' ').split(' ');
+    let sum = 0;
+    for (let i = 0; i < qntpalavras.length; i++) {
+      if (isNaN(qntpalavras[i]) === false && qntpalavras[i] !== '') {
+        sum += 1;
+      }
+
+    }
+    return sum;
   },
+
   getNumberSum: (text) => {
     //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
+    const qntpalavras = text.replace(/.$/, ' ').split(' ');
+    let sum = 0;
+    for (let i = 0; i < qntpalavras.length; i++) {
+      if (isNaN(qntpalavras[i]) === false) {
+        sum += Number(qntpalavras[i]);
+      }
+
+    }
+    return sum;
   },
 };
 
 export default analyzer;
+
--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 import analyzer from './analyzer.js';
 
-//TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
+const textarea = document.querySelector('[name="user-input"]');
+
+const wordcount = document.querySelector('[data-testid = "word-count"]');
+const charactercount = document.querySelector('[data-testid = "character-count"]');
+const characternospacescount = document.querySelector('[data-testid = "character-no-spaces-count"]');
+const numbercount = document.querySelector('[data-testid = "number-count"]');
+const numbersum = document.querySelector('[data-testid = "number-sum"]');
+const wordlengthaverage = document.querySelector('[data-testid = "word-length-average"]');
+
+textarea.addEventListener("input", analise);
+
+function analise(){
+  const text = textarea.value;
+
+  wordcount.innerHTML = "Palavras: " + analyzer.getWordCount(text);
+  charactercount.innerHTML = "Caracteres: " + analyzer.getCharacterCount(text);
+  characternospacescount.innerHTML = "Caracteres excluindo espaços e sinais de pontuação: " + analyzer.getCharacterCountExcludingSpaces(text);
+  numbercount.innerHTML = "Números: " + analyzer.getNumberCount(text);
+  numbersum.innerHTML = "Soma total de números: " + analyzer.getNumberSum(text);
+  wordlengthaverage.innerHTML = "Comprimento médio das palavras: " + analyzer.getAverageWordLength(text);
+}
+
+document.getElementById('reset-button').addEventListener('click', () => {
+  textarea.value = '';
+  analise();
+});
