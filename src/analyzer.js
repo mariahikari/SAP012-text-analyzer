376c1f542daa98fccaae1c6e683ea011d564e4da
mariahikari <mariahikariyps@gmail.com>
2024-01-22 18:19:16 -0300

    Comentários do Código

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,60 +1,53 @@
+const analyzer = { //Funções para analisar o texto
 
-
-const analyzer = {
-  getWordCount: (text) => {
-    //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
-    const qntpalavras = text.trim().split(' '); 
-    if (text === ""){
-      return 0
+  getWordCount: (text) => { //Esta função retornará a quantidade de palavras em uma string
+    const qntpalavras = text.trim().split(' '); //Remove os espaços em branco da string usando trim() e divide a string em palavras usando split(' ')
+    if (text === "") { //Verificando se a string está vazia 
+      return 0 //Se a string estiver vazia retornará 0
     }
-    return qntpalavras.length;
+    return qntpalavras.length; //Caso contrário, retornará o comprimento do array de palavras
 
   },
 
-  getCharacterCount: (text) => {
-    //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
-    return text.length;
+  getCharacterCount: (text) => { //Esta função retornará a quantidade de caracteres em uma string
+    return text.length; //Retorna o comprimento da string 
   },
-  getCharacterCountExcludingSpaces: (text) => {
-    //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
-    const excluindoespaco = text.replace(/[\s.:,;!]/g, '');
-    return excluindoespaco.length;
 
+  getCharacterCountExcludingSpaces: (text) => { //Esta função retornará a quantidade de caracteres excluindo espaços e sinais de pontuação em uma string
+    const excluindoespaco = text.replace(/[\s.:,;!]/g, ''); //Remove espaços em branco e ".:,;!" com a RegExp .replace(/[\s.:,;!]/g, '')
+    return excluindoespaco.length; //Retorna o comprimento da string sem espaços em branco e ".:,;!"
   },
-  getAverageWordLength: (text) => {
-    //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
-    const qntpalavras = text.split(' ');
+
+  getAverageWordLength: (text) => { //Esta função retornará a média do comprimento das palavras em uma string
+    const qntpalavras = text.split(' '); //Divide a string em palavras
     let sum = 0;
     for (let i = 0; i < qntpalavras.length; i++) {
-      sum += qntpalavras[i].length;
+      sum += qntpalavras[i].length; //Calcula a soma dos comprimentos das palavras
     }
 
-    const comprimentomedio = (sum / qntpalavras.length).toFixed(2);
-    return Number(comprimentomedio);
-
+    const comprimentomedio = (sum / qntpalavras.length).toFixed(2); //Divide a soma pelo número de palavras para encontrar a média, em seguida, o resultado é formatado com duas casas decimais usando toFixed(2)
+    return Number(comprimentomedio); //Retorna convertido para número 
   },
-  getNumberCount: (text) => {
-    //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
-    const qntpalavras = text.replace(/.$/, ' ').split(' ');
+
+  getNumberCount: (text) => { //Esta função retornará a contagem de números de uma string 
+    const qntpalavras = text.replace(/.$/, ' ').split(' '); //Substitui todos os caracteres que não são números por espaços com a RegExp .replace(/.$/, ' ') e divide a string em palavras usando split(' ')
     let sum = 0;
-    for (let i = 0; i < qntpalavras.length; i++) {
-      if (isNaN(qntpalavras[i]) === false && qntpalavras[i] !== '') {
-        sum += 1;
+    for (let i = 0; i < qntpalavras.length; i++) { //Loop para checar se no array de palavras há números 
+      if (isNaN(qntpalavras[i]) === false && qntpalavras[i] !== '') { //Verifica se o elemento é um número e se a string não está vazia
+        sum += 1; //Incrementa a contagem se for um número
       }
-
     }
     return sum;
   },
 
-  getNumberSum: (text) => {
-    //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
-    const qntpalavras = text.replace(/.$/, ' ').split(' ');
+  getNumberSum: (text) => { //Esta função retornará a soma de todos os números de uma string
+    //Similar ao getNumberCount, mas em vez de incrementar a contagem, incrementa a soma com o valor numérico da palavra
+    const qntpalavras = text.replace(/.$/, ' ').split(' '); 
     let sum = 0;
     for (let i = 0; i < qntpalavras.length; i++) {
       if (isNaN(qntpalavras[i]) === false) {
-        sum += Number(qntpalavras[i]);
+        sum += Number(qntpalavras[i]); 
       }
-
     }
     return sum;
   },
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import analyzer from './analyzer.js';
+import analyzer from './analyzer.js'; // Importa o módulo analyzer do arquivo "analyzer.js" que contém as funções de analise do testo
 
+//As constantes abaixo selecionam os elementos do DOM usando document.querySelector, tais elementos correspondem a componentes na página HTML com os resultados da análise de texto
 const textarea = document.querySelector('[name="user-input"]');
-
 const wordcount = document.querySelector('[data-testid = "word-count"]');
 const charactercount = document.querySelector('[data-testid = "character-count"]');
 const characternospacescount = document.querySelector('[data-testid = "character-no-spaces-count"]');
@@ -9,11 +9,12 @@ const numbercount = document.querySelector('[data-testid = "number-count"]');
 const numbersum = document.querySelector('[data-testid = "number-sum"]');
 const wordlengthaverage = document.querySelector('[data-testid = "word-length-average"]');
 
-textarea.addEventListener("input", analise);
+textarea.addEventListener("input", analise); //Adiciona um ouvinte de evento à textarea e o evento input é acionado sempre que o conteúdo da textarea é alterado
 
-function analise(){
-  const text = textarea.value;
-
+function analise(){ //Esta função será chamada sempre que o usuário digitar no textarea
+  const text = textarea.value; //Obtém o valor do texto do textarea
+  
+  //Abaixo são chamadas as funções do "analyzer" para analisar o texto e o conteúdo dos elementos no DOM com os resultados da análise são atualizados
   wordcount.innerHTML = "Palavras: " + analyzer.getWordCount(text);
   charactercount.innerHTML = "Caracteres: " + analyzer.getCharacterCount(text);
   characternospacescount.innerHTML = "Caracteres excluindo espaços e sinais de pontuação: " + analyzer.getCharacterCountExcludingSpaces(text);
@@ -22,7 +23,7 @@ function analise(){
   wordlengthaverage.innerHTML = "Comprimento médio das palavras: " + analyzer.getAverageWordLength(text);
 }
 
-document.getElementById('reset-button').addEventListener('click', () => {
+document.getElementById('reset-button').addEventListener('click', () => { //Adiciona um ouvinte de evento ao botão reset que, quando clicado, limpa o conteúdo do textarea 
   textarea.value = '';
-  analise();
+  analise(); //Redefine os resultados da análise do texto
 });
